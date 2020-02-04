556bd38819f524e1387df8a7e72b476c83c482db
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-04 05:47:16 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,14 @@
 class Customer
   attr_accessor :name, :age
 
-  @all = [] 
+  @all = []
 
 
-  def initialize
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
 
 
 
